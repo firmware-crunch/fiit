9eef4f88200dcec9b1aac554c979b7425c392ea1
VincentDary <VincentDary@users.noreply.github.com>
2026-03-20 10:23:38 +0100

    Improve test script readability and maintenance
    Move test files names to a bash array, to avoid errors when add a new
    test file, to allow commenting a test file name, and to add better
    readability of the script.
    
    Previously the test file names were specified directly on the command
    line which makes difficult to read the script commands, and it was not
    possible to comment a test file.

--- tests/run.sh
+++ tests/run.sh
@@ -2,7 +2,7 @@
 
 ################################################################################
 #
-# Copyright 2022-2025 Vincent Dary
+# Copyright 2022-2026 Vincent Dary
 #
 # This file is part of fiit.
 #
@@ -21,57 +21,63 @@
 #
 ################################################################################
 
+TEST_FILES=(
+
+test_utils.py
+test_logger.py
+test_config_loader.py
+
+test_machine_defines.py
+test_machine_memory.py
+test_machine_registers.py
+test_machine_cpu.py
+test_machine_machine.py
+
+test_dbg_disasm.py
+test_dbg_defines.py
+test_dbg_dbg.py
+test_dbg_factory.py
+
+test_emunicorn_registers.py
+test_emunicorn_memory.py
+test_emunicorn_cpu.py
+test_emunicorn_fix.py
+test_emunicorn_arm32_coproc.py
+test_emunicorn_arm32_cpu.py
+test_emunicorn_factory.py
+test_emunicorn_dbg.py
+
+test_dev_arm32.py
+test_dev_pl190.py
+test_dev_factory.py
+
+test_ctypesarch_arch_arm32.py
+test_ctypesarch_defines.py
+test_ctypesarch_cdata.py
+test_ctypesarch_config.py
+test_ctypesarch_translator.py
+
+test_iotrace_mmio_filter.py
+
+test_shell_front_dbg.py
+test_shell_front_machine.py
+test_shell_shell.py
+
+test_plugin.py
+test_plugin_logger.py
+test_plugin_machine.py
+test_plugin_shell.py
+
+test_hooking_cc_aapcs32.py
+)
+
+#
 # IPY_TEST_SIMPLE_PROMPT=1 Force IPython to use input than readline.
+#
 IPY_TEST_SIMPLE_PROMPT=1 JUPYTER_PLATFORM_DIRS=1 pytest                        \
   --cov-report html                                                            \
   --cov="fiit"                                                                 \
   -s -v "${@}"                                                                 \
-                                                                               \
-test_utils.py                                                                  \
-test_logger.py                                                                 \
-test_config_loader.py                                                          \
-                                                                               \
-test_machine_defines.py                                                        \
-test_machine_memory.py                                                         \
-test_machine_registers.py                                                      \
-test_machine_cpu.py                                                            \
-test_machine_machine.py                                                        \
-                                                                               \
-test_dbg_disasm.py                                                             \
-test_dbg_defines.py                                                            \
-test_dbg_dbg.py                                                                \
-test_dbg_factory.py                                                            \
-                                                                               \
-test_emunicorn_registers.py                                                    \
-test_emunicorn_memory.py                                                       \
-test_emunicorn_cpu.py                                                          \
-test_emunicorn_fix.py                                                          \
-test_emunicorn_arm32_coproc.py                                                 \
-test_emunicorn_arm32_cpu.py                                                    \
-test_emunicorn_factory.py                                                      \
-test_emunicorn_dbg.py                                                          \
-                                                                               \
-test_dev_arm32.py                                                              \
-test_dev_pl190.py                                                              \
-test_dev_factory.py                                                            \
-                                                                               \
-test_ctypesarch_arch_arm32.py                                                  \
-test_ctypesarch_defines.py                                                     \
-test_ctypesarch_cdata.py                                                       \
-test_ctypesarch_config.py                                                      \
-test_ctypesarch_translator.py                                                  \
-                                                                               \
-test_iotrace_mmio_filter.py                                                    \
-                                                                               \
-test_shell_front_dbg.py                                                        \
-test_shell_front_machine.py                                                    \
-test_shell_shell.py                                                            \
-                                                                               \
-test_plugin.py                                                                 \
-test_plugin_logger.py                                                          \
-test_plugin_machine.py                                                         \
-test_plugin_shell.py                                                           \
-                                                                               \
-test_hooking_cc_aapcs32.py
+  "${TEST_FILES[@]}"
 
 rm -rf .pytest_cache
