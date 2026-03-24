b8475d9ed6348c3f172874dd7c47f8f911f5e023
VincentDary <VincentDary@users.noreply.github.com>
2026-03-24 21:23:30 +0100

    Update python package name in setup.py

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="fiit-python",
+    name="fiit-py",
     version="0.3.0",
     author="Vincent Dary",
     author_email="",
